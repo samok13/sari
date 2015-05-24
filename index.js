07bbc5d273e760d5632cfef0fc674c0cfb65600d
Sam O'Keefe <samok13@gmail.com>
2015-05-23 19:14:26 -0700

    Adding "good" plugin to listen, log, and print http requests.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var Hapi = require('hapi');
+var Good = require('good');
+var GoodConsole = require('good-console');
 
 //Create a service with a host and port
 var server = new Hapi.Server();
@@ -15,7 +17,25 @@ server.route({
 	}
 });
 
-//Start the server
-server.start(function () {
-    console.log('Server running at:', server.info.uri);
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: GoodConsole,
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; 
+    // Something bad happened loading the plugin
+    }
+
+	//If all ok, Start the server
+	server.start(function () {
+   		console.log('Server running at:', server.info.uri);
+	});
 });
